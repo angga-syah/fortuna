884020306a56e1b1043e53ae096c660731ee1914
angga-syah <134121694+angga-syah@users.noreply.github.com>
2024-11-24 11:42:22 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".payment-form").addEventListener("submit", function(e) {
-    const paymentMethod = document.getElementById("payment-method").value;
-    if (!paymentMethod) {
-        e.preventDefault();
-        alert("Please select a payment method.");
-    }
-});
